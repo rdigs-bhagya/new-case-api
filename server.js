85597fb7d3908bfe84416d0d3f07e413ee405655
bhagyashree patil <115216811+BHAGU2001@users.noreply.github.com>
2026-06-19 17:43:21 +0530

    serverless code done

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+const serverless = require('serverless-http');
 const http = require("http");
 const app = require("./index");
 
-const port = process.env.PORT || 5000;
-const host = "0.0.0.0"; // <-- Important for Render
+if (require.main === module) {
+  const port = process.env.PORT || 5000;
+  const host = "0.0.0.0"; // <-- Important for Render
 
-const server = http.createServer(app);
+  const server = http.createServer(app);
 
-server.listen(port, host, () => {
-  console.log(`🚀 App is running on http://${host}:${port}`);
-});
+  server.listen(port, host, () => {
+    console.log(`🚀 App is running locally on http://${host}:${port}`);
+  });
+}
+
+module.exports.handler = serverless(app);
